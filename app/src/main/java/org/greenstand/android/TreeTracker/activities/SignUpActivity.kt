38fb0a195d932dfb9277370ed47c4649120874a7
LaviniaDragunoi <lavinia.dragunoi@yahoo.ro>
2019-02-08 01:12:32 +0200

    feat: Added new layout for new design
    Added new layout files for the new design. Added layouts only for
    Login screen

---
+++ app/src/main/java/org/greenstand/android/TreeTracker/activities/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package org.greenstand.android.TreeTracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.greenstand.android.TreeTracker.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
