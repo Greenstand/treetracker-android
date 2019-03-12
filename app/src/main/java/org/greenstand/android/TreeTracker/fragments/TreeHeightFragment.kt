985d82dbefbb42e92c41d7e7f05bee7d33802304
Lawrence Muller <lawrencejmuller@gmail.com>
2019-03-11 21:59:39 -0400

    Add ViewModel and Lifecycle dependencies

--- app/src/main/java/org/greenstand/android/TreeTracker/fragments/TreeHeightFragment.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/fragments/TreeHeightFragment.kt
@@ -5,9 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.android.UI
 import org.greenstand.android.TreeTracker.R
+import kotlin.coroutines.CoroutineContext
 
 class TreeHeightFragment : Fragment() {
+
+    lateinit var viewModel: TreeHeightViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel = ViewModelProviders.of(this).get(TreeHeightViewModelImpl::class.java)
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_tree_height, container, false)
@@ -15,4 +30,31 @@ class TreeHeightFragment : Fragment() {
 
 
 
+}
+
+
+
+interface TreeHeightViewModel {
+
+}
+
+class TreeHeightViewModelImpl : CoroutineViewModel(), TreeHeightViewModel {
+
+
+
+}
+
+
+
+
+abstract class CoroutineViewModel : ViewModel(), CoroutineScope {
+
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = UI
+
+    override fun onCleared() {
+        job.cancel()
+    }
 }
