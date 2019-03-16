5ece9e8ae48b15867c3ed66957869122bdbcce0f
Lawrence Muller <lawrencejmuller@gmail.com>
2019-03-15 21:18:51 -0400

    wip type conversion

--- app/src/main/java/org/greenstand/android/TreeTracker/database/AppDatabase.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/database/AppDatabase.kt
@@ -1,15 +1,11 @@
 package org.greenstand.android.TreeTracker.database
 
-import android.content.ContentValues
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
+import android.database.Cursor
 import androidx.room.Database
-import androidx.room.DatabaseConfiguration
-import androidx.room.InvalidationTracker
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import androidx.sqlite.db.SupportSQLiteOpenHelper
 import org.greenstand.android.TreeTracker.database.entities.*
 import androidx.room.Room
 
@@ -53,7 +49,34 @@ abstract class AppDatabase : RoomDatabase() {
 class MigrationV1ToV2 : Migration(1, 2) {
 
     override fun migrate(database: SupportSQLiteDatabase) {
+        // WIP to convert Y/N boolean values to integers for Room to use
+        database.beginTransaction()
 
+        val isSyncedInts: List<Pair<Long, Int>> = database.loadBooleans(TreeEntity.TABLE, TreeEntity.IS_SYNCED)
+
+        database.endTransaction()
     }
 
+    fun <T> SupportSQLiteDatabase.loadColumn(table: String, column: String, block: Cursor.(column: String) -> T): List<Pair<Long, T>> {
+        val cursor = this.query("""
+            SELECT _id, $column FROM $table;
+        """.trimIndent())
+
+        val columnValues = mutableListOf<Pair<Long, T>>()
+        while(cursor.moveToNext()) {
+            columnValues.add(cursor.loadLong("_id") to cursor.block(column))
+        }
+        return columnValues
+    }
+
+    fun SupportSQLiteDatabase.loadBooleans(table: String, column: String): List<Pair<Long, Int>> {
+        return loadColumn(table, column) {
+            if (loadString(it) == "Y") 1 else 0
+        }
+    }
 }
+
+fun Cursor.loadString(id: String): String? = getString(getColumnIndex(id))
+fun Cursor.loadFloat(id: String): Float = getFloat(getColumnIndex(id))
+fun Cursor.loadLong(id: String): Long = getLong(getColumnIndex(id))
+fun Cursor.loadDouble(id: String): Double = getDouble(getColumnIndex(id))
