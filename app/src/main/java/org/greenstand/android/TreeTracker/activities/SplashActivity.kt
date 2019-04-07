2a3062ecf331bd7107fa3064d01a407949538f54
Elforama <elforama@gmail.com>
2019-04-07 14:22:23 -0400

    Cleanup

--- app/src/main/java/org/greenstand/android/TreeTracker/activities/SplashActivity.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/activities/SplashActivity.kt
@@ -3,44 +3,35 @@ package org.greenstand.android.TreeTracker.activities
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-import org.greenstand.android.TreeTracker.R
-import org.greenstand.android.TreeTracker.utilities.ValueHelper
 import org.greenstand.android.TreeTracker.BuildConfig
+import org.greenstand.android.TreeTracker.R
 
 import timber.log.Timber
 
 class SplashActivity : Activity() {
 
-    var mSplashThread: Thread? = null
-
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_splash)
-        Timber.tag("BuildVariant").d("build variant: " + BuildConfig.BUILD_TYPE + ", url: "
-                + BuildConfig.BASE_URL)
+        Timber.tag("BuildVariant").d("build variant: ${BuildConfig.BUILD_TYPE}, url: ${BuildConfig.BASE_URL}")
 
+        GlobalScope.launch {
+            delay(1000)
 
-        // The thread to wait for splash screen events
-        mSplashThread = object : Thread() {
-            override fun run() {
-                try {
-                    Thread.sleep(ValueHelper.SPLASH_SCREEN_DURATION)
-                } catch (ex: InterruptedException) {
-                }
-
-                // Run next activity
-                val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-                startActivity(intent)
-                overridePendingTransition(0, 0)
-
-                finish()
-                overridePendingTransition(0, 0)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or
+                                 Intent.FLAG_ACTIVITY_CLEAR_TASK or
+                                 Intent.FLAG_ACTIVITY_NO_ANIMATION)
             }
-        }
 
-        mSplashThread?.start()
+            overridePendingTransition(0, 0)
+            startActivity(intent)
+            overridePendingTransition(0, 0)
+        }
     }
 }
