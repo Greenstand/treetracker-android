66014c35f82ecf0beda8977f175b86a300f7b006
Zaven <matthew.fullstackgis@gmail.com>
2019-05-28 00:00:20 -0700

    Give them the 500 tree long click in debug/test

--- app/src/main/java/org/greenstand/android/TreeTracker/managers/FeatureFlags.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/managers/FeatureFlags.kt
@@ -3,7 +3,7 @@ package org.greenstand.android.TreeTracker.managers
 import org.greenstand.android.TreeTracker.BuildConfig
 
 object FeatureFlags {
-    val DEBUG_ENABLED: Boolean = BuildConfig.BUILD_TYPE == "dev"
+    val DEBUG_ENABLED: Boolean = BuildConfig.BUILD_TYPE == "dev" || BuildConfig.BUILD_TYPE == "debug"
     val TREE_HEIGHT_FEATURE_ENABLED: Boolean = BuildConfig.TREE_HEIGHT_FEATURE_ENABLED
     val TREE_NOTE_FEATURE_ENABLED: Boolean = BuildConfig.TREE_NOTE_FEATURE_ENABLED
     val FABRIC_ENABLED: Boolean = BuildConfig.ENABLE_FABRIC
