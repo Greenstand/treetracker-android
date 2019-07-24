b6a8478f76908bfbab4fbff1f592c0b2565cbaf2
Zaven <matthew.fullstackgis@gmail.com>
2019-07-23 22:34:30 -0700

    Revert "Implement uploads via AWS S3 with transfer acceleration"
    This reverts commit 754ccfbd7fa881a818a39e233a06898a4189ae16.

--- app/src/main/java/org/greenstand/android/TreeTracker/api/DOSpaces.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/api/DOSpaces.kt
@@ -2,19 +2,9 @@ package org.greenstand.android.TreeTracker.api
 
 import android.annotation.SuppressLint
 import com.amazonaws.AmazonClientException
-import com.amazonaws.AmazonServiceException
-import com.amazonaws.SdkClientException
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.internal.StaticCredentialsProvider
-
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.services.s3.model.BucketAccelerateConfiguration
-import com.amazonaws.services.s3.model.BucketAccelerateStatus
-import com.amazonaws.services.s3.model.GetBucketAccelerateConfigurationRequest
-import com.amazonaws.services.s3.model.SetBucketAccelerateConfigurationRequest
-
+import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.AccessControlList
 import com.amazonaws.services.s3.model.GroupGrantee
 import com.amazonaws.services.s3.model.Permission
@@ -34,65 +24,12 @@ import kotlin.String.Companion
  */
 
 class DOSpaces private constructor() {
-
-    private var s3Client: AmazonS3? = null
+    private val s3Client: AmazonS3
 
     init {
         val basicAWSCredentials = BasicAWSCredentials(BuildConfig.DOS_ACCESS_KEY, BuildConfig.DOS_SECRET_KEY)
-        val bucketName = BuildConfig.DO_SPACES_BUCKET
-        val credentialsProvider = StaticCredentialsProvider(basicAWSCredentials)
-
-        if(BuildConfig.ENABLE_TRANSFER_ACCELERATION) {
-
-            val clientRegion = BuildConfig.DO_SPACES_ENDPOINT
-
-            try {
-                // Create an Amazon S3 client that is configured to use the accelerate endpoint.
-                s3Client = AmazonS3ClientBuilder.standard()
-                    .withRegion(clientRegion)
-                    .withCredentials(credentialsProvider)
-                    .enableAccelerateMode()
-                    .build();
-
-                // Enable Transfer Acceleration for the specified bucket.
-                s3Client?.setBucketAccelerateConfiguration(
-                    SetBucketAccelerateConfigurationRequest (bucketName,
-                    BucketAccelerateConfiguration (
-                            BucketAccelerateStatus.Enabled
-                    )
-                ));
-
-                // Verify that transfer acceleration is enabled for the bucket.
-                val accelerateStatus = s3Client?.getBucketAccelerateConfiguration (
-                         GetBucketAccelerateConfigurationRequest (bucketName))
-                    ?.status
-                System.out.println("Bucket accelerate status: " + accelerateStatus);
-
-                // Upload a new object using the accelerate endpoint.
-                //s3Client?.putObject(bucketName, keyName, "Test object for transfer acceleration");
-                //System.out.println("Object \"" + keyName + "\" uploaded with transfer acceleration.");
-            } catch (e : AmazonServiceException) {
-                // The call was transmitted successfully, but Amazon S3 couldn't process
-                // it, so it returned an error response.
-                e.printStackTrace();
-            } catch (e : SdkClientException) {
-                // Amazon S3 couldn't be contacted for a response, or the client
-                // couldn't parse the response from Amazon S3.
-                e.printStackTrace();
-            }
-
-        } else {
-
-            s3Client = AmazonS3ClientBuilder.standard()
-                .withCredentials(credentialsProvider)
-                .build()
-            s3Client?.setEndpoint(String.format("https://%s.digitaloceanspaces.com/", BuildConfig.DO_SPACES_ENDPOINT))
-
-        }
-
-
-
-
+        s3Client = AmazonS3Client(basicAWSCredentials)
+        s3Client.setEndpoint(String.format("https://%s.digitaloceanspaces.com/", BuildConfig.DO_SPACES_ENDPOINT))
     }
 
     @SuppressLint("SimpleDateFormat")
@@ -107,7 +44,7 @@ class DOSpaces private constructor() {
         val dosKey = timeStamp + '_'.toString() + UUID.randomUUID() + '_'.toString() + image.name
         val poRequest = PutObjectRequest(BuildConfig.DO_SPACES_BUCKET, dosKey, image)
         poRequest.withAccessControlList(acl)
-        val poResult = s3Client?.putObject(poRequest)
+        val poResult = s3Client.putObject(poRequest)
         return String.format("https://%s.%s.digitaloceanspaces.com/%s",  BuildConfig.DO_SPACES_BUCKET,
             BuildConfig.DO_SPACES_ENDPOINT, dosKey)
     }
