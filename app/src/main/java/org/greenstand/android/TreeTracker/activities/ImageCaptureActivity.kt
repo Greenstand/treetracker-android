169e7cb1d3b220d921ce75d8827f3d0ba8c79ce8
Elforama <elforama@gmail.com>
2019-11-17 22:10:14 -0500

    Remove old camera screen and update new camera with same UI

--- app/src/main/java/org/greenstand/android/TreeTracker/activities/ImageCaptureActivity.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/activities/ImageCaptureActivity.kt
@@ -3,12 +3,10 @@ package org.greenstand.android.TreeTracker.activities
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.graphics.Matrix
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.Surface
 import android.view.TextureView
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import org.greenstand.android.TreeTracker.R
@@ -17,17 +15,17 @@ import org.greenstand.android.TreeTracker.utilities.ImageUtils
 import org.greenstand.android.TreeTracker.utilities.ValueHelper
 import timber.log.Timber
 import java.io.File
-import java.nio.ByteBuffer
 
 class ImageCaptureActivity : AppCompatActivity() {
 
     private lateinit var viewFinder: TextureView
     private lateinit var imageCaptureButton: ImageButton
+    private lateinit var toolbarTitle: TextView
 
     companion object {
-        private val SELFIE_MODE = "SELFIE_MODE"
+        private const val SELFIE_MODE = "SELFIE_MODE"
 
-        fun createIntent(context: Context, selfieMode: Boolean = true): Intent {
+        fun createIntent(context: Context, selfieMode: Boolean = false): Intent {
             return Intent(context, ImageCaptureActivity::class.java).apply {
                 putExtra(SELFIE_MODE, selfieMode)
             }
@@ -39,12 +37,20 @@ class ImageCaptureActivity : AppCompatActivity() {
 
         setContentView(R.layout.fragment_camera)
 
+        toolbarTitle = findViewById(R.id.toolbar_title)
         viewFinder = findViewById(R.id.view_finder)
         imageCaptureButton = findViewById(R.id.capture_button)
 
         val bundle = intent.extras
 
         val captureSelfie = bundle?.getBoolean(SELFIE_MODE, false) ?: false
+
+
+        if (captureSelfie) {
+            toolbarTitle.text= getString(R.string.take_a_selfie)
+        } else {
+            toolbarTitle.text = getString(R.string.add_a_tree)
+        }
 
         viewFinder.post { startCamera(captureSelfie) }
     }
@@ -56,8 +62,6 @@ class ImageCaptureActivity : AppCompatActivity() {
 
         // Create configuration object for the image capture use case
         val imageCaptureConfig = ImageCaptureConfig.Builder()
-            //.setTargetAspectRatio(Rational(1, 1))
-            //.setTargetResolution(Size(800, 800))
             .setLensFacing(lensFacing)
             .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
             .build()
@@ -93,18 +97,6 @@ class ImageCaptureActivity : AppCompatActivity() {
                                      })
         }
 
-//        val imageAnalysisConfig = ImageAnalysisConfig.Builder()
-//            //.setTargetResolution(Size(1280, 720))
-//            .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
-//            .build()
-//        val imageAnalysis = ImageAnalysis(imageAnalysisConfig)
-//
-//        imageAnalysis.setAnalyzer({ image: ImageProxy, rotationDegrees: Int ->
-//                                      val buffer = image.planes[0].buffer
-//                                      val imageByteArray = buffer.toByteArray()
-//                                      val data = cropByteArray(imageByteArray, Rect(0, 0, 500, 500))
-//                                  })
-
         CameraX.bindToLifecycle(this, preview, imageCapture)
     }
 
@@ -113,36 +105,10 @@ class ImageCaptureActivity : AppCompatActivity() {
         val lensFacing = if (captureSelfie) CameraX.LensFacing.FRONT else CameraX.LensFacing.BACK
 
         val previewConfig = PreviewConfig.Builder()
-            //.setTargetAspectRatio(Rational(1, 1))
-            //.setTargetRotation(Surface.ROTATION_0)
-            //.setTargetResolution(Size(800, 800))
             .setLensFacing(lensFacing)
             .build()
 
-        val preview = AutoFitPreviewBuilder.build(previewConfig, viewFinder)
-
-        return preview
-    }
-
-    private fun updateTransform() {
-        val matrix = Matrix()
-
-        // Compute the center of the view finder
-        val centerX = viewFinder.width / 2f
-        val centerY = viewFinder.height / 2f
-
-        // Correct preview output to account for display rotation
-        val rotationDegrees = when(viewFinder.display.rotation) {
-            Surface.ROTATION_0 -> 0
-            Surface.ROTATION_90 -> 90
-            Surface.ROTATION_180 -> 180
-            Surface.ROTATION_270 -> 270
-            else -> return
-        }
-        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
-
-        // Finally, apply transformations to our TextureView
-        viewFinder.setTransform(matrix)
+        return AutoFitPreviewBuilder.build(previewConfig, viewFinder)
     }
 
     private fun testFocusQuality(imageFile: File): Double {
@@ -158,43 +124,6 @@ class ImageCaptureActivity : AppCompatActivity() {
             println(e)
         }
         // on an error, we return very bad focus.
-        return 0.0;
+        return 0.0
     }
-}
-
-private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
-    private var lastAnalyzedTimestamp = 0L
-
-
-
-    override fun analyze(image: ImageProxy, rotationDegrees: Int) {
-
-        val buffer = image.planes[0].buffer
-        val imageByteArray = buffer.toByteArray()
-        val data = cropByteArray(imageByteArray, Rect(0, 0, 500, 500))
-
-    }
-}
-
-private fun ByteBuffer.toByteArray(): ByteArray {
-    rewind()
-    val data = ByteArray(remaining())
-    get(data)
-    return data
-}
-
-private fun cropByteArray(array : ByteArray, cropRect: Rect): ByteArray {
-    val croppedArray = ByteArray(cropRect.width()*cropRect.height())
-    val imageWidth = 640
-    var i = 0
-    array.forEachIndexed { index, byte ->
-        val x = index % imageWidth
-        val y = index / imageWidth
-
-        if (cropRect.left <= x && x < cropRect.right && cropRect.top <= y && y < cropRect.bottom) {
-            croppedArray[i] = byte
-            i++
-        }
-    }
-    return croppedArray
 }
