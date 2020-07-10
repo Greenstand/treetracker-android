1a19d361da8f67203c0e6deca151b3443d336789
Arun Bakt <arunbakt@gmail.com>
2020-07-10 13:44:14 -0700

    WIP -- debugging travis build

--- properties.sh
+++ properties.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -ex
-echo "Installing property files..."
 if [ '$TRAVIS_PULL_REQUEST' != "false"]; then
-  echo "About to touch treetracker.keys.proerties"
-  touch treetracker.keys.properites
+  echo "Generating dummy property file for the pull request build"
+  #touch treetracker.keys.properites
 else
   openssl aes-256-cbc -K $encrypted_env_key -iv $encrypted_env_iv -in .env.enc -out fastlane/.env -d
   openssl aes-256-cbc -K $encrypted_props_key -iv $encrypted_props_iv -in treetracker.keys.properties.enc -out treetracker.keys.properties -d
