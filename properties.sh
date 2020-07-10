39a7f629a172de80337604212fb5f4e308a6c7c9
Arun Bakt <arunbakt@gmail.com>
2020-07-10 13:18:35 -0700

    Update travis file to allow PR builds to succeed without depending on encrypted property values

---
+++ properties.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -ex
+if [ '$TRAVIS_PULL_REQUEST' != "false"]; then
+  touch treetracker.keys.properites
+else
+  openssl aes-256-cbc -K $encrypted_env_key -iv $encrypted_env_iv -in .env.enc -out fastlane/.env -d
+  openssl aes-256-cbc -K $encrypted_props_key -iv $encrypted_props_iv -in treetracker.keys.properties.enc -out treetracker.keys.properties -d
+fi
