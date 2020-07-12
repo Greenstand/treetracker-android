ae83f10326de4980f3e13e2fb9d608b17112b624
Arun Bakt <arunbakt@gmail.com>
2020-07-11 23:40:37 -0700

    Fix a typo in the name of the property file created

--- properties.sh
+++ properties.sh
@@ -4,7 +4,7 @@ set -ex
 
 if [ "$TRAVIS_PULL_REQUEST" != false ]; then
   echo "Generating dummy property file for the pull request build"
-  touch treetracker.keys.properites
+  touch treetracker.keys.properties
 else
   openssl aes-256-cbc -K $encrypted_env_key -iv $encrypted_env_iv -in .env.enc -out fastlane/.env -d
   openssl aes-256-cbc -K $encrypted_props_key -iv $encrypted_props_iv -in treetracker.keys.properties.enc -out treetracker.keys.properties -d
