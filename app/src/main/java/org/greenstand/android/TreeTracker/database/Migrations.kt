d1e3e9dc9a195eb06e480fd60784f2bb23699139
Arun Bakt <arunbakt@gmail.com>
2020-07-23 23:32:52 -0700

    WIP -- fix bugs in the migration (sql typos, test pending

--- app/src/main/java/org/greenstand/android/TreeTracker/database/Migrations.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/database/Migrations.kt
@@ -9,15 +9,99 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
 
         try {
             database.beginTransaction()
+
+            // Create version 4 entities (tables)
+            val createPlanterInfo = """CREATE TABLE IF NOT EXISTS `planter_info` (
+                | `_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                | `planter_identifier` TEXT NOT NULL,
+                | `first_name` TEXT NOT NULL,
+                | `last_name` TEXT NOT NULL,
+                | `organization` TEXT,
+                |  `phone` TEXT,
+                |  `email` TEXT,
+                |  `latitude` REAL NOT NULL,
+                |  `longitude` REAL NOT NULL,
+                |  `uploaded` INTEGER NOT NULL,
+                |  `created_at` INTEGER NOT NULL,
+                |  `bundle_id` TEXT
+            )""".trimMargin()
+            database.execSQL(createPlanterInfo)
+            val createIndexPlanterIdentifier =  """
+                | CREATE INDEX IF NOT EXISTS 
+                | `index_planter_identifier` ON `planter_info` (`planter_identifier`)
+                | """.trimMargin()
+            database.execSQL(createIndexPlanterIdentifier)
+            val createIndexUploaded = """
+                | CREATE INDEX IF NOT EXISTS `index_planter_uploaded`
+                | ON `planter_info` (`uploaded`)
+                | """.trimMargin()
+            database.execSQL(createIndexUploaded)
+
+            val createPlanterCheckIn = """CREATE TABLE IF NOT EXISTS `planter_check_in` (
+                | `_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                | `planter_info_id` INTEGER NOT NULL,
+                | `local_photo_path` TEXT,
+                | `photo_url` TEXT,
+                | `latitude` REAL NOT NULL,
+                | `longitude` REAL NOT NULL,
+                | `created_at` INTEGER NOT NULL,
+                | FOREIGN KEY(`planter_info_id`) REFERENCES `planter_info`(`_id`)
+                | ON UPDATE CASCADE ON DELETE NO ACTION
+                | )""".trimMargin()
+            database.execSQL(createPlanterCheckIn)
+
+            val createIndexPlanterInfo =  """CREATE INDEX IF NOT EXISTS
+                |`index_planter_check_in_planter_info_id`
+                | ON `planter_check_in` (`planter_info_id`)""".trimMargin()
+            database.execSQL(createIndexPlanterInfo)
+
+            val createTreeCapture = """CREATE TABLE IF NOT EXISTS `tree_capture` (
+                | `_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                | `uuid` TEXT NOT NULL,
+                | `planter_checkin_id` INTEGER NOT NULL,
+                | `local_photo_path` TEXT,
+                | `photo_url` TEXT,
+                | `note_content` TEXT NOT NULL,
+                | `latitude` REAL NOT NULL,
+                | `longitude` REAL NOT NULL,
+                | `accuracy` REAL NOT NULL,
+                | `uploaded` INTEGER NOT NULL,
+                | `created_at` INTEGER NOT NULL,
+                | `bundle_id` TEXT,
+                | FOREIGN KEY(`planter_checkin_id`) 
+                | REFERENCES `planter_check_in`(`_id`) ON UPDATE CASCADE ON DELETE NO ACTION )
+                | """.trimMargin()
+            database.execSQL(createTreeCapture)
+
+            val createPlanterCheckInIndex = """CREATE INDEX IF NOT EXISTS
+                | `index_tree_capture_planter_checkin_id` ON `tree_capture` (`planter_checkin_id`)
+                | """.trimMargin()
+            database.execSQL(createPlanterCheckInIndex)
+
+            val createTreeAttribute = """CREATE TABLE IF NOT EXISTS `tree_attribute` (
+                | `_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                | `key` TEXT NOT NULL, `value` TEXT NOT NULL,
+                | `tree_capture_id` INTEGER NOT NULL,
+                | FOREIGN KEY(`tree_capture_id`) REFERENCES `tree_capture`(`_id`)
+                | ON UPDATE CASCADE ON DELETE NO ACTION )
+                | """.trimMargin()
+            database.execSQL(createTreeAttribute)
+
+            val createIndexTreeCapture = """CREATE INDEX IF NOT EXISTS 
+                |`index_tree_attribute_tree_capture_id` 
+                | ON `tree_attribute` (`tree_capture_id`)
+                | """.trimMargin()
+            database.execSQL(createIndexTreeCapture)
+
             // Populate Planter registrations
             val populatePlanterInfoEntity  = """INSERT INTO `planter_info` (
-            | `_id`, `planter_identifier`, `first_name`, `last_name`, `organization`,
-            | `phone`, `email`, `latitude`, `longitude`, `uploaded`, `created_at`)
-            | SELECT `_id`, `identifier`, `first_name`, `last_name`, `organization`,
-            | `phone`, `email`, substr(`location`, 0, instr(`location`, ',')), 
-            |  substr(`location`, instr(`location`, ',')+1), `uploaded`, 
-            |  strftime('%s', `time_created`) FROM `planter_details`
-            | """.trimMargin()
+                | `_id`, `planter_identifier`, `first_name`, `last_name`, `organization`,
+                | `phone`, `email`, `latitude`, `longitude`, `uploaded`, `created_at`)
+                | SELECT `_id`, `identifier`, `first_name`, `last_name`, `organization`,
+                | `phone`, `email`, substr(`location`, 0, instr(`location`, ',')), 
+                |  substr(`location`, instr(`location`, ',')+1), `uploaded`, 
+                |  strftime('%s', `time_created`) FROM `planter_details`
+                | """.trimMargin()
             database.execSQL(populatePlanterInfoEntity)
 
             // Populate planter checkins
@@ -25,7 +109,7 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
             | `_id`, `planter_info_id`, `local_photo_path`, `photo_url`,
             | `latitude`, `longitude`, `created_at`) SELECT `_id`, `planter_details_id`,
             | `photo_path`, `photo_url`, substr(`location`, 0, instr(`location`, ',')),
-            | substr(`location`, instr(`location`, ',')+1), strftime('%s', `time_created`)
+            | substr(`location`, instr(`location`, ',')+1), `time_created`
             | FROM `planter_identifications`
         """.trimMargin()
             database.execSQL(populatePlanterCheckIns)
@@ -35,7 +119,7 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
             | `planter_checkin_id`, `local_photo_path`, `note_content`,
             | `latitude`, `longitude`, `accuracy`, `uploaded`, `created_at`) SELECT
             | `tree`.`_id`, `tree`.`uuid`, `tree`.`planter_identification_id`, `photo`.`name`,
-            | `note`.`content`, `location`.`latitude`, `location`.`longitude`,
+            | `note`.`content`, `location`.`lat`, `location`.`long`,
             | `location`.`accuracy`, `tree`.`is_synced`, strftime('%s', `tree`.`time_created`)
             | FROM `tree`
             | LEFT OUTER JOIN `location` ON `location`.`_id` = `tree`.`location_id`
@@ -45,7 +129,7 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
             | LEFT OUTER JOIN `note` ON `note`.`_id` = `tree_note`.`note_id`
             | LEFT OUTER JOIN `planter_identifications`
             | ON `planter_identifications`.`_id` = `tree`.`planter_identification_id`
-        """.trimMargin()
+            | """.trimMargin()
             database.execSQL(populateTreeCaptures)
 
             val populateTreeAttributes = """INSERT INTO `tree_attribute` (
