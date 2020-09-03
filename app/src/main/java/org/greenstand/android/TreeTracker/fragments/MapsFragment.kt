1c45a70f207af569cd86326be60a222fff964d47
Arun Bakt <arunbakt@gmail.com>
2020-09-03 12:49:38 -0700

    Add a label to indicate location convergence to the user

--- app/src/main/java/org/greenstand/android/TreeTracker/fragments/MapsFragment.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/fragments/MapsFragment.kt
@@ -12,7 +12,6 @@ import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
