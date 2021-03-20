0e155b15150fd47cf37cc054e5137a71fc678ad9
Jonathan Muller <elforama@gmail.com>
2021-03-20 15:46:21 -0400

    Create basic functioning org picker UI (#591)
    Co-authored-by: Jonathan Muller <lawrencejmuller@gmail.com>

--- app/src/main/java/org/greenstand/android/TreeTracker/orgpicker/OrgPickerScreen.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/orgpicker/OrgPickerScreen.kt
@@ -1,22 +1,78 @@
 package org.greenstand.android.TreeTracker.orgpicker
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import org.greenstand.android.TreeTracker.models.Org
 
 @Composable
-fun OrgPickerScreen(viewModel: OrgPickerViewModel) {
+fun OrgPickerScreen(viewModel: OrgPickerViewModel, navController: NavController) {
+
+    val state by viewModel.state.observeAsState(OrgPickerState())
+
+    val lifecycle = LocalLifecycleOwner.current
+    LaunchedEffect(lifecycle) {
+        viewModel.uiEvents.observe(lifecycle) {
+            when(it) {
+                OrgPickerUIEvents.Exit -> navController.popBackStack()
+            }
+        }
+    }
+
+    Scaffold {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            item {
+                Text(
+                    text = "Org Picker",
+                )
+                Spacer(modifier = Modifier.height(100.dp))
+            }
+            items(state.orgs) { org ->
+                OrgItem(
+                    org = org,
+                    isSelected = org == state.currentOrg,
+                    onClick = {
+                        viewModel.setOrg(org)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun OrgItem(org: Org, isSelected: Boolean, onClick: () -> Unit) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .clickable(onClick = onClick)
+            .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Org Picker",
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = if (isSelected) Color.DarkGray else Color.Gray)
         )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(org.name)
     }
 }
--- app/src/main/java/org/greenstand/android/TreeTracker/orgpicker/OrgPickerViewModel.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/orgpicker/OrgPickerViewModel.kt
@@ -1,10 +1,45 @@
 package org.greenstand.android.TreeTracker.orgpicker
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.greenstand.android.TreeTracker.models.Org
 import org.greenstand.android.TreeTracker.models.Organizations
 
+data class OrgPickerState(
+    val orgs: List<Org> = emptyList(),
+    val currentOrg: Org? = null
+)
+
+sealed class OrgPickerUIEvents {
+    object Exit : OrgPickerUIEvents()
+}
+
 class OrgPickerViewModel(
-    private val organizations: Organizations
+    private val organizations: Organizations,
 ) : ViewModel() {
 
+    private val _state = MutableLiveData(OrgPickerState())
+    val state: LiveData<OrgPickerState> = _state
+
+    private val _uiEvents = MutableLiveData<OrgPickerUIEvents>()
+    val uiEvents: LiveData<OrgPickerUIEvents> = _uiEvents
+
+    init {
+        viewModelScope.launch {
+            _state.value = OrgPickerState(
+                orgs = organizations.getOrgs(),
+                currentOrg = organizations.getCurrentOrg()
+            )
+        }
+    }
+
+    fun setOrg(org: Org) {
+        viewModelScope.launch {
+            organizations.setCurrentOrg(org)
+            _uiEvents.value = OrgPickerUIEvents.Exit
+        }
+    }
 }
