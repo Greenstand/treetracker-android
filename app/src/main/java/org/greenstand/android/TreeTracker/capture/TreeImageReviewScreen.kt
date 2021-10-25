055619d486e138df3e46b4ef7a8e66ddce8e4fb9
Taofeek Oduola <oduola.taofeekkola@gmail.com>
2021-10-25 11:42:19 +0100

    Fix merge conflicts

--- app/src/main/java/org/greenstand/android/TreeTracker/capture/TreeImageReviewScreen.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/capture/TreeImageReviewScreen.kt
@@ -1,28 +1,23 @@
 package org.greenstand.android.TreeTracker.capture
 
-import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Checkbox
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.material.slider.Slider
 import kotlinx.coroutines.launch
 import org.greenstand.android.TreeTracker.R
 import org.greenstand.android.TreeTracker.activities.CaptureImageContract
@@ -31,13 +26,10 @@ import org.greenstand.android.TreeTracker.root.LocalNavHostController
 import org.greenstand.android.TreeTracker.root.LocalViewModelFactory
 import org.greenstand.android.TreeTracker.view.*
 
-private val openDialog =  mutableStateOf(false)
-
 @Composable
 fun TreeImageReviewScreen(
     photoPath: String,
     viewModel: TreeImageReviewViewModel = viewModel(factory = LocalViewModelFactory.current)
-
 ) {
 
     val state by viewModel.state.observeAsState(TreeImageReviewState())
@@ -51,9 +43,7 @@ fun TreeImageReviewScreen(
                     modifier = Modifier
                         .align(Alignment.Center)
                         .size(width = 100.dp, 60.dp),
-                    onClick = {
-                        openDialog.value = true
-                    }
+                    onClick = { }
                 ) {
                     Text("NOTE")
                 }
@@ -63,65 +53,62 @@ fun TreeImageReviewScreen(
         bottomBar = {
             ActionBar(
                 centerAction = {
-            Row(
-                horizontalArrangement = Arrangement.Center
-            ) {
-                DepthButton(
-                    onClick = {
-                        navController.navigate(NavRoute.Selfie.route) {
-                            navController.popBackStack()
+                    Row(
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        DepthButton(
+                            onClick = {
+                                navController.navigate(NavRoute.Selfie.route) {
+                                    navController.popBackStack()
+                                }
+                            },
+                            colors = DepthButtonColors(
+                                color = AppColors.Red,
+                                shadowColor = AppColors.RedShadow,
+                                disabledColor = AppColors.RedShadow,
+                                disabledShadowColor = AppColors.RedShadow
+                            ),
+                            modifier = Modifier.size(width = 54.dp, height = 54.dp)
+                                .align(Alignment.CenterVertically)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.cancel_image),
+                                contentDescription = null,
+                                modifier = Modifier.size(width = 54.dp, height = 54.dp)
+                                    .align(Alignment.Center)
+                            )
                         }
-                    },
-                    colors = DepthButtonColors(
-                        color = AppColors.Red,
-                        shadowColor = AppColors.RedShadow,
-                        disabledColor = AppColors.RedShadow,
-                        disabledShadowColor = AppColors.RedShadow
-                    ),
-                    modifier = Modifier
-                        .size(width = 54.dp, height = 54.dp)
-                        .align(Alignment.CenterVertically)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.cancel_image),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(width = 54.dp, height = 54.dp)
-                            .align(Alignment.Center)
-                    )
-                }
-                Spacer(modifier = Modifier.width(30.dp))
-                DepthButton(
-                    onClick = {
-                        navController.navigate(NavRoute.Selfie.route) {
-                            navController.popBackStack()
-                        }
-                    },
-                    colors = DepthButtonColors(
-                        color = AppColors.Red,
-                        shadowColor = AppColors.RedShadow,
-                        disabledColor = AppColors.RedShadow,
-                        disabledShadowColor = AppColors.RedShadow
-                    ),
-                    modifier = Modifier
-                        .size(width = 54.dp, height = 54.dp)
-                        .align(Alignment.CenterVertically)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.check_icon),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(width = 54.dp, height = 54.dp)
-                            .align(Alignment.Center)
-                    )
-                }
+                        Spacer(modifier = Modifier.width(30.dp))
+                        DepthButton(
+                            onClick = {
+                                scope.launch {
+                                    viewModel.approveImage()
+                                    navController.popBackStack()
+                                }
+                            },
+                            colors = DepthButtonColors(
+                                color = Color.Green,
+                                shadowColor = AppColors.GreenShadow,
+                                disabledColor = AppColors.GreenDisabled,
+                                disabledShadowColor = AppColors.GreenShadowDisabled
+                            ),
+                            modifier = Modifier.size(width = 54.dp, height = 54.dp)
+                                .align(Alignment.CenterVertically)
 
-            }
-        }
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.check_icon),
+                                contentDescription = null,
+                                modifier = Modifier.size(width = 54.dp, height = 54.dp)
+                                    .align(Alignment.Center)
+                            )
+                        }
+
+                    }
+                }
             )
         }
     ) {
-        addNoteDialog()
         LocalImage(
             modifier = Modifier.fillMaxSize(),
             imagePath = photoPath,
@@ -129,61 +116,4 @@ fun TreeImageReviewScreen(
             contentScale = ContentScale.Fit
         )
     }
-
 }
-@Composable
-fun addNoteDialog() {
-    val treeTrackerViewModel: TreeCaptureViewModel =
-        viewModel(factory = TreeCaptureViewModelFactory(""))
-    var text by remember { mutableStateOf("") }
-
-    if (openDialog.value) {
-        AlertDialog(
-            onDismissRequest = {
-                openDialog.value = false
-            },
-            title = {
-                Text(text = "Add Note to tree")
-            },
-            text = {
-                TextField(
-                    value = text,
-                    onValueChange = { text = it },
-
-                    )
-            },
-            buttons = {
-                Row(
-                    modifier = Modifier
-                        .padding(all = 8.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    DepthButton(
-                        onClick = {
-                            treeTrackerViewModel.addNote(text)
-                            openDialog.value = false
-                        },
-                        colors = DepthButtonColors(
-                            color = Color.Green,
-                            shadowColor = AppColors.GreenShadow,
-                            disabledColor = AppColors.GreenDisabled,
-                            disabledShadowColor = AppColors.GreenShadowDisabled
-                        ),
-                        modifier = Modifier
-                            .size(width = 54.dp, height = 54.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.check_icon),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(width = 54.dp, height = 54.dp)
-                                .align(Alignment.Center)
-                        )
-                    }
-                }
-            }
-        )
-    }
-}
-
