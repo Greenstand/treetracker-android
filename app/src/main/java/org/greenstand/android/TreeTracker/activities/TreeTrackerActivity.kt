140ccf58eb23aa7276292a576d28d7c914580330
Jonathan Muller <elforama@gmail.com>
2021-12-08 22:11:30 -0500

    Complete networking layer for messages endpoint

--- app/src/main/java/org/greenstand/android/TreeTracker/activities/TreeTrackerActivity.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/activities/TreeTrackerActivity.kt
@@ -4,33 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.ExperimentalComposeApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.greenstand.android.TreeTracker.models.FeatureFlags
 import org.greenstand.android.TreeTracker.models.Language
 import org.greenstand.android.TreeTracker.models.LanguageSwitcher
 import org.greenstand.android.TreeTracker.models.TreeTrackerViewModelFactory
-import org.greenstand.android.TreeTracker.models.messages.MessagesRepo
 import org.greenstand.android.TreeTracker.root.Root
 import org.koin.android.ext.android.inject
-import timber.log.Timber
 
 class TreeTrackerActivity : ComponentActivity() {
 
     private val languageSwitcher: LanguageSwitcher by inject()
     private val viewModelFactory: TreeTrackerViewModelFactory by inject()
-    private val messagesRepo: MessagesRepo by inject()
 
     @ExperimentalComposeApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        GlobalScope.launch {
-            Timber.d("JONATHAN START")
-            messagesRepo.getMessages("admin")
-                .forEach {
-                    Timber.d("JONATHAN $it")
-                }
-        }
+
         if (FeatureFlags.USE_SWAHILI) {
             languageSwitcher.setLanguage(Language.SWAHILI, resources)
         } else {
--- app/src/main/java/org/greenstand/android/TreeTracker/models/messages/MessagesRepo.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/models/messages/MessagesRepo.kt
@@ -3,16 +3,11 @@ package org.greenstand.android.TreeTracker.models.messages
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.greenstand.android.TreeTracker.models.messages.network.MessagesApiService
-import timber.log.Timber
 
 class MessagesRepo(private val apiService: MessagesApiService) {
 
     suspend fun getMessages(wallet: String): List<Message> = withContext(Dispatchers.IO) {
-        Timber.d("JONATHAN START API")
         val result = apiService.getMessages(wallet)
-        Timber.d("JONATHAN START CONVERT")
-        val r = result.messages.map { it.toMessage() }
-        Timber.d("JONATHAN START DONE")
-        r
+        return@withContext result.messages.map { it.toMessage() }
     }
 }
