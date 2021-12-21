a5203aed75694fc89b27d0dd40b097dcaee4e1ff
Taofeek’s MacBook Pro <oduola.taofeekkola@gmail.com>
2021-12-21 18:31:22 +0100

    Added missing import statements.

--- app/src/main/java/org/greenstand/android/TreeTracker/signup/CredentialEntryView.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/signup/CredentialEntryView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.AlertDialog
