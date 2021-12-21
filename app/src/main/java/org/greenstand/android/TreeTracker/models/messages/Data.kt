9f95ed7a242b6f96a5cb8215a2a89757c766b999
Jonathan Muller <elforama@gmail.com>
2021-12-20 20:01:29 -0500

    create base interface for message data

--- app/src/main/java/org/greenstand/android/TreeTracker/models/messages/Data.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/models/messages/Data.kt
@@ -1,22 +1,29 @@
 package org.greenstand.android.TreeTracker.models.messages
 
-data class Message(
-    val from: String,
-    val to: String,
+interface Message {
+    val from: String
+    val to: String
+    val composedAt: String
+}
+
+data class TextMessage(
+    override val from: String,
+    override val to: String,
+    override val composedAt: String,
     val subject: String,
     val body: String,
-    val composedAt: String,
     val parentMessageId: String?,
-    val videoLink: String?,
-    val survey: Survey?,
-)
+) : Message
 
-data class Survey(
-    val questions: List<Question>?,
-    val answers: List<String>?,
-)
+data class SurveyMessage(
+    override val from: String,
+    override val to: String,
+    override val composedAt: String,
+    val questions: List<Question>,
+    val answers: List<String>,
+) : Message
 
 data class Question(
-    val prompt: String?,
-    val choices: List<String>?,
+    val prompt: String,
+    val choices: List<String>,
 )
