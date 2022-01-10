914d0ac94cb8e943047306312ed1eec606d55c63
Taofeek Oduola <oduola.taofeekkola@gmail.com>
2022-01-10 20:22:29 +0100

    Fix backstack navigation for profile icon #661 (#745)
    * Fixes #661 issue
    
    * Formatted code
    
    Co-authored-by: Jonathan Muller <elforama@gmail.com>

--- app/src/main/java/org/greenstand/android/TreeTracker/walletselect/WalletSelectScreen.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/walletselect/WalletSelectScreen.kt
@@ -57,13 +57,20 @@ fun WalletSelectScreen(
                             modifier = Modifier
                                 .width(100.dp)
                                 .height(100.dp)
-                                .padding(start = 15.dp,
-                                         top= 10.dp,
-                                         end =10.dp,
-                                         bottom = 10.dp)
+                                .padding(
+                                    start = 15.dp,
+                                    top = 10.dp,
+                                    end = 10.dp,
+                                    bottom = 10.dp
+                                )
                                 .aspectRatio(1.0f)
                                 .clip(RoundedCornerShape(percent = 10))
-                                .clickable { navController.navigate(NavRoute.UserSelect.route) },
+                                .clickable {
+                                    navController.navigate(NavRoute.UserSelect.route) {
+                                        popUpTo(NavRoute.Dashboard.route)
+                                        launchSingleTop = true
+                                    }
+                                },
                             imagePath = it,
                             contentScale = ContentScale.Crop,
                         )
@@ -100,11 +107,14 @@ fun WalletSelectScreen(
         },
     ) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
-                .padding(start = 10.dp,
-                         top= 10.dp,
-                         end =10.dp,
-                         bottom = 90.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    start = 10.dp,
+                    top = 10.dp,
+                    end = 10.dp,
+                    bottom = 90.dp
+                )
         ) {
             state.currentUser?.let { currentUser ->
                 item {
@@ -140,10 +150,12 @@ fun WalletItem(user: User, isSelected: Boolean, onClick: (Long) -> Unit) {
                         modifier = Modifier
                             .height(150.dp)
                             .width(150.dp)
-                            .padding(start = 20.dp,
-                                     top= 10.dp,
-                                     end =10.dp,
-                                     bottom = 30.dp)
+                            .padding(
+                                start = 20.dp,
+                                top = 10.dp,
+                                end = 10.dp,
+                                bottom = 30.dp
+                            )
             )
 
             UserButton(
