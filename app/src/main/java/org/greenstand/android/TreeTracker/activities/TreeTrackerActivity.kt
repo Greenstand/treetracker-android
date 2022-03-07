ae194cee30451d3867426cf68957fc95a8a1870a
Taofeek’s MacBook Pro <oduola.taofeekkola@gmail.com>
2022-03-07 19:19:45 +0100

    Check for GPS device in the phone and show dialog if not present.

--- app/src/main/java/org/greenstand/android/TreeTracker/activities/TreeTrackerActivity.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/activities/TreeTrackerActivity.kt
@@ -1,15 +1,26 @@
 package org.greenstand.android.TreeTracker.activities
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ExperimentalComposeApi
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import org.greenstand.android.TreeTracker.R
 import org.greenstand.android.TreeTracker.models.FeatureFlags
 import org.greenstand.android.TreeTracker.models.Language
 import org.greenstand.android.TreeTracker.models.LanguageSwitcher
 import org.greenstand.android.TreeTracker.models.TreeTrackerViewModelFactory
 import org.greenstand.android.TreeTracker.root.Root
 import org.greenstand.android.TreeTracker.theme.CustomTheme
+import org.greenstand.android.TreeTracker.view.AppColors
+import org.greenstand.android.TreeTracker.view.CustomDialog
 import org.koin.android.ext.android.inject
 
 class TreeTrackerActivity : ComponentActivity() {
@@ -26,11 +37,36 @@ class TreeTrackerActivity : ComponentActivity() {
         } else {
             languageSwitcher.applyCurrentLanguage(this)
         }
-
-        setContent {
-            CustomTheme {
-                Root(viewModelFactory)
+        if (hasGPSDevice(this)) {
+            setContent {
+                CustomTheme {
+                    Root(viewModelFactory)
+                }
+            }
+        } else {
+            setContent {
+                Surface(color = AppColors.Gray) {
+                    CustomTheme {
+                        NoGPSDeviceDialog(onPositiveClick = { finishAndRemoveTask() })
+                    }
+                }
             }
         }
     }
+
+    fun hasGPSDevice(context: Context): Boolean {
+        val mgr = context.getSystemService(LOCATION_SERVICE) as LocationManager
+        val providers = mgr.allProviders
+        return providers.contains(LocationManager.GPS_PROVIDER)
+    }
+}
+
+@Composable
+fun NoGPSDeviceDialog(onPositiveClick: () -> Unit){
+    CustomDialog(
+        dialogIcon = painterResource(id = R.drawable.error_outline),
+        title = stringResource(R.string.no_gps_device_header),
+        textContent = stringResource(R.string.no_gps_device_content),
+        onPositiveClick = onPositiveClick
+    )
 }
