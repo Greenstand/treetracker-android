4e2a51dd0d41a65c1a636bd3dbe0c2a694a68078
Taofeek’s MacBook Pro <oduola.taofeekkola@gmail.com>
2022-03-13 22:47:26 +0100

    Updated individual message list to use real data models

--- app/src/main/java/org/greenstand/android/TreeTracker/messages/individualmeassagelist/IndividualMessageListViewModel.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/messages/individualmeassagelist/IndividualMessageListViewModel.kt
@@ -6,9 +6,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import org.greenstand.android.TreeTracker.models.Users
+import org.greenstand.android.TreeTracker.models.messages.Message
+import org.greenstand.android.TreeTracker.models.messages.network.responses.MessageResponse
+import org.greenstand.android.TreeTracker.models.messages.network.responses.MessagesResponse
 import org.greenstand.android.TreeTracker.models.user.User
+import java.util.*
 
 data class IndividualMessageListState(
+    val messages: List<Message> = Collections.emptyList(),
     val currentUser: User? = null,
 )
 
@@ -18,14 +23,10 @@ class IndividualMessageListViewModel(
     private val _state = MutableLiveData<IndividualMessageListState>()
     val state: LiveData<IndividualMessageListState> = _state
 
-
     fun loadPlanter(planterInfoId: Long) {
         viewModelScope.launch {
             val currentUser = users.getUser(planterInfoId)
             _state.value = IndividualMessageListState(currentUser = currentUser)
         }
     }
-
-
-
 }
