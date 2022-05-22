3ac95aed159fa6beaf0aa75a0d171a15e86afc59
Taofeek’s MacBook Pro <oduola.taofeekkola@gmail.com>
2022-05-22 23:20:05 +0100

    Modified capturing dialog to show closer to the bottom(Requested PR changes)

--- app/src/main/java/org/greenstand/android/TreeTracker/capture/TreeCaptureScreen.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/capture/TreeCaptureScreen.kt
@@ -3,21 +3,21 @@ package org.greenstand.android.TreeTracker.capture
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -31,7 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -57,7 +56,6 @@ import org.greenstand.android.TreeTracker.view.DepthButton
 import org.greenstand.android.TreeTracker.view.DepthSurfaceShape
 import org.greenstand.android.TreeTracker.view.TreeCaptureTutorial
 import org.greenstand.android.TreeTracker.view.UserImageButton
-import org.greenstand.android.TreeTracker.view.showLoadingSpinner
 
 @ExperimentalPermissionsApi
 @Composable
@@ -212,30 +210,53 @@ fun TreeCaptureScreen(
 @Composable
 fun CaptureCustomLoading(isLoading: Boolean, progress: Float) {
     if (isLoading) {
-        CustomDialog(
-            dialogIcon = null,
-            backgroundModifier = Modifier
-                .alpha(0.9f)
-                .padding(2.dp),
-            title = stringResource(R.string.tracking_progress_header),
-            textContent = stringResource(R.string.tracking_progress_message),
-            content = {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    CircularProgressIndicator(
-                        modifier = Modifier
-                            .size(height = 80.dp, width = 80.dp)
-                            .padding(top = 10.dp,),
-                        color = AppColors.Green,
-                    )
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Text(
-                        text = "${progress.times(100).toInt()} ${"%"}",
-                        color = CustomTheme.textColors.primaryText,
-                        style = CustomTheme.typography.medium,
-                        fontWeight = FontWeight.Bold,
-                    )
-                }
+        Box(
+            modifier = Modifier
+                .padding(bottom = 90.dp)
+                .fillMaxSize(), contentAlignment = Alignment.BottomCenter
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(20.dp)
+                    .background(color = AppColors.Gray, shape = RoundedCornerShape(percent = 10))
+                    .alpha(0.8f)
+                    .border(1.dp, color = AppColors.Green, shape = RoundedCornerShape(percent = 10))
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Text(
+                    text = stringResource(R.string.tracking_progress_header),
+                    color = CustomTheme.textColors.primaryText,
+                    style = CustomTheme.typography.medium,
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Text(
+                    text = stringResource(R.string.tracking_progress_message),
+                    color = CustomTheme.textColors.primaryText,
+                    style = CustomTheme.typography.medium,
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(height = 80.dp, width = 80.dp)
+                        .padding(top = 10.dp),
+                    color = AppColors.Green,
+                )
+                Spacer(modifier = Modifier.height(15.dp))
+                Text(
+                    text = "${progress.times(100).toInt()} ${"%"}",
+                    color = CustomTheme.textColors.primaryText,
+                    style = CustomTheme.typography.medium,
+                    fontWeight = FontWeight.Bold,
+                )
             }
-        )
+        }
     }
 }
