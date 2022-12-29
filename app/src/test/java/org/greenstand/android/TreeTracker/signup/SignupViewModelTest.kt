346d2e17741d8f5c831a3cac5c7adc01a0cb0790
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2022-12-29 21:37:36 +0300

    Added live data utl test and fake file generator

--- app/src/test/java/org/greenstand/android/TreeTracker/signup/SignupViewModelTest.kt
+++ app/src/test/java/org/greenstand/android/TreeTracker/signup/SignupViewModelTest.kt
@@ -1,4 +1,68 @@
 package org.greenstand.android.TreeTracker.signup
 
+import com.google.common.truth.Truth
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.greenstand.android.TreeTracker.models.UserRepo
+import org.greenstand.android.TreeTracker.usecases.CheckForInternetUseCase
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest=Config.NONE)
 class SignupViewModelTest {
+
+    private val userRepo = mockk<UserRepo>(relaxed = true)
+    private val checkForInternetUseCase = mockk<CheckForInternetUseCase>(relaxed = true)
+    private val signupViewModel = SignupViewModel(userRepo, checkForInternetUseCase)
+
+    @Test
+    fun `update first name, returns valid first name`() = runTest {
+        signupViewModel.updateFirstName("caleb")
+        Truth.assertThat(signupViewModel.state.value?.firstName).isNotNull()
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
