30a07198becd0c125fe2ef6f6df2bc6f2fddc7a5
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-01-12 15:52:19 +0300

    Added OrganizationPickerViewModelTest

--- app/src/test/java/org/greenstand/android/TreeTracker/orgpicker/OrgPickerViewModelTest.kt
+++ app/src/test/java/org/greenstand/android/TreeTracker/orgpicker/OrgPickerViewModelTest.kt
@@ -1,4 +1,48 @@
 package org.greenstand.android.TreeTracker.orgpicker
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.greenstand.android.TreeTracker.MainCoroutineRule
+import org.greenstand.android.TreeTracker.models.organization.OrgRepo
+import org.greenstand.android.TreeTracker.utils.FakeFileGenerator
+import org.greenstand.android.TreeTracker.utils.getOrAwaitValueTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
 class OrgPickerViewModelTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private val orgRepo = mockk<OrgRepo>(relaxed = true)
+    private lateinit var orgPickerViewModel: OrgPickerViewModel
+
+    @Before
+    fun setup(){
+        orgPickerViewModel = OrgPickerViewModel(orgRepo)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `set fake organization, returns success with correct data`()= runBlockingTest {
+        val currentOrg = FakeFileGenerator.fakeOrganization
+        //Given
+        coEvery { orgRepo.currentOrg() } returns  currentOrg
+
+        // When
+        orgPickerViewModel.setOrg(FakeFileGenerator.fakeOrganization)
+
+        //Assert LiveData has correct data
+        val result = orgPickerViewModel.state.getOrAwaitValueTest().currentOrg
+        Assert.assertEquals(result, FakeFileGenerator.fakeOrganization)
+    }
+
 }
