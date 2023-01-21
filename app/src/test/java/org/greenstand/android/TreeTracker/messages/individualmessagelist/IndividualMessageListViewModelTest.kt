8159c65c0ea56e55cdd14410169213563f4ae920
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-01-21 10:15:00 +0300

    Updated unit tests

--- app/src/test/java/org/greenstand/android/TreeTracker/messages/individualmessagelist/IndividualMessageListViewModelTest.kt
+++ app/src/test/java/org/greenstand/android/TreeTracker/messages/individualmessagelist/IndividualMessageListViewModelTest.kt
@@ -5,12 +5,15 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.greenstand.android.TreeTracker.MainCoroutineRule
 import org.greenstand.android.TreeTracker.messages.individualmeassagelist.IndividualMessageListViewModel
 import org.greenstand.android.TreeTracker.models.UserRepo
 import org.greenstand.android.TreeTracker.models.messages.MessagesRepo
 import org.greenstand.android.TreeTracker.utils.FakeFileGenerator
+import org.greenstand.android.TreeTracker.utils.getOrAwaitValueTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -30,6 +33,9 @@ class IndividualMessageListViewModelTest {
     @Before
     fun setup(){
         coEvery { userRepo.getUser(any()) } returns FakeFileGenerator.emptyUser
+        coEvery { messagesRepo.getMessageFlow(any()) } returns flow {
+            listOf(FakeFileGenerator.fakeAnnouncementMessage)
+        }
         individualMessageListViewModel = IndividualMessageListViewModel(userId, userRepo, messagesRepo)
     }
 
@@ -37,4 +43,17 @@ class IndividualMessageListViewModelTest {
     fun `verify user repo gets the correct user`() = runBlocking {
         coVerify { userRepo.getUser(userId) }
     }
+
+    @Test
+    fun `verify message repo gets the correct message flow`() = runBlocking {
+        val currentUser = userRepo.getUser(userId)
+        val id = currentUser!!.wallet
+        coVerify { messagesRepo.getMessageFlow(id) }
+    }
+
+    @Test
+    fun `WHEN selected message, state gets updated`() = runBlocking {
+        val message = FakeFileGenerator.fakeAnnouncementMessage
+    }
+
 }
