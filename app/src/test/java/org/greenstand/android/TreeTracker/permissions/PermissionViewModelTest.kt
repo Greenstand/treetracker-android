75d4ea437f7679e61bb19f89338efe905bad9a2c
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-02-05 23:34:18 +0300

    added unit test for the permissionViewModel

--- app/src/test/java/org/greenstand/android/TreeTracker/permissions/PermissionViewModelTest.kt
+++ app/src/test/java/org/greenstand/android/TreeTracker/permissions/PermissionViewModelTest.kt
@@ -2,11 +2,16 @@ package org.greenstand.android.TreeTracker.permissions
 
 import android.location.LocationManager
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.greenstand.android.TreeTracker.MainCoroutineRule
+import org.greenstand.android.TreeTracker.utils.getOrAwaitValueTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class PermissionViewModelTest {
@@ -23,5 +28,14 @@ class PermissionViewModelTest {
     @Before
     fun setup(){
         testSubject = PermissionViewModel(locationManager)
+    }
+
+    @Test
+    fun `verify isLocationEnabled sets the correct value in the permission Items state`()= runBlocking {
+        val provider = LocationManager.GPS_PROVIDER
+        coEvery { locationManager.isProviderEnabled(provider)} returns true
+        testSubject.isLocationEnabled()
+        val result = testSubject.state.getOrAwaitValueTest().isLocationEnabled!!
+        Assert.assertTrue(result)
     }
 }
