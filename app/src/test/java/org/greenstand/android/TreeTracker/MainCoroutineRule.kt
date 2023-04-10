a3ed56f6dfb0b1f98bcc01a78cadd03c7e02eaf3
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-04-10 09:01:14 +0300

    removed wildcard imports

--- app/src/test/java/org/greenstand/android/TreeTracker/MainCoroutineRule.kt
+++ app/src/test/java/org/greenstand/android/TreeTracker/MainCoroutineRule.kt
@@ -2,7 +2,10 @@ package org.greenstand.android.TreeTracker
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.*
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.resetMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
