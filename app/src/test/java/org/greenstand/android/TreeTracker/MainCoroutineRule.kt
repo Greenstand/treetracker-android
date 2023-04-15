9c1e906a2c6ba90963cc3ea4894aa325ced0b41d
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-04-15 19:30:54 +0300

    MainCoroutineRule cleanup (#1085)
    * MainCoroutineRule Cleanup
    
    * removed unnecessary file
    
    * removed wildcard imports

--- app/src/test/java/org/greenstand/android/TreeTracker/MainCoroutineRule.kt
+++ app/src/test/java/org/greenstand/android/TreeTracker/MainCoroutineRule.kt
@@ -2,26 +2,22 @@ package org.greenstand.android.TreeTracker
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.resetMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
-
 @ExperimentalCoroutinesApi
-class MainCoroutineRule(val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()):
-    TestWatcher(),
-    TestCoroutineScope by TestCoroutineScope(dispatcher) {
-    override fun starting(description: Description?) {
-        super.starting(description)
-        Dispatchers.setMain(dispatcher)
+class MainCoroutineRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
     }
 
-    override fun finished(description: Description?) {
-        super.finished(description)
-        cleanupTestCoroutines()
+    override fun finished(description: Description) {
         Dispatchers.resetMain()
     }
 }
