5f04bc31bb7a006bf58a8115e7ab9d378011f4cb
bearismybrother <thetwai123456789@gmail.com>
2026-06-24 19:35:48 -0700

    Updated ExceptionLogger rules and added test cases

--- app/src/main/java/org/greenstand/android/TreeTracker/analytics/ExceptionLogger.kt
+++ app/src/main/java/org/greenstand/android/TreeTracker/analytics/ExceptionLogger.kt
@@ -20,6 +20,20 @@ import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
+/**
+ * Custom Timber Tree that logs messages to Firebase Crashlytics.
+ *
+ * Rule table:
+ * - Timber.i / Timber.w -> breadcrumb only
+ * - Timber.e(message) -> breadcrumb only
+ * - Timber.e(throwable, message) -> breadcrumb + recordException(throwable)
+ * - Timber.v / Timber.d -> ignored
+ *
+ * Tips:
+ * - Don't log Personally Identifiable Information
+ * - Only record and send to Crashlytics through this class
+ * - Timber.e("...") without throwable doesn't always mean it's an exception.
+ */
 class ExceptionLogger : Timber.Tree() {
     override fun log(
         priority: Int,
@@ -27,11 +41,17 @@ class ExceptionLogger : Timber.Tree() {
         message: String,
         throwable: Throwable?,
     ) {
-        if (priority == Log.VERBOSE || priority == Log.DEBUG) {
-            return
-        }
-        val error = throwable ?: Exception(message)
+        when (priority) {
+            Log.INFO -> Firebase.crashlytics.log("[$tag]: $message")
+            Log.ERROR -> {
+                Firebase.crashlytics.log("[$tag]: $message")
+                if (throwable != null) {
+                    Firebase.crashlytics.recordException(throwable)
+                }
+            }
 
-        Firebase.crashlytics.recordException(error)
+            Log.ASSERT -> Firebase.crashlytics.log("[$tag]: $message")
+            Log.VERBOSE, Log.DEBUG -> return
+        }
     }
 }
